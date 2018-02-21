e4febb9b4f092f11bafc4cfa1c9652c9d1aa9efc
Wei Zheng Chua <wzchua77@gmail.com>
2018-02-21 14:53:27 +0800

    Update filtering to handle normal. Updated FilterOctree module

--- GlobalIllumination/FilterOctree.cpp
+++ GlobalIllumination/FilterOctree.cpp
@@ -4,15 +4,30 @@
 
 void FilterOctree::initialize()
 {
+    if (hasInitialized) {
+        return;
+    }
 
     filterOctreeShader.generateShader("./Shaders/MIPmapOctree.comp", ShaderProgram::COMPUTE);
     filterOctreeShader.linkCompileValidate();
 }
 
-void FilterOctree::run(GLBufferObject<NodeStruct>& nodeOctree, GLBufferObject<GLuint> & ssboLeafIndexList, GLuint textureColor, GLuint textureNormal)
+void FilterOctree::run(GLBufferObject<CounterBlock>& counterSet, GLBufferObject<NodeStruct>& nodeOctree, GLBufferObject<GLuint> & ssboLeafIndexList, GLuint textureColor, GLuint textureNormal, GLuint leafCount)
 {
+    GLuint currentShaderProgram = filterOctreeShader.use();
+
+    counterSet.bind(1);
+    nodeOctree.bind(2);
+    ssboLeafIndexList.bind(3);
     glBindImageTexture(4, textureColor, 0, GL_TRUE, 0, GL_READ_WRITE, GL_R32UI);
     glBindImageTexture(5, textureNormal, 0, GL_TRUE, 0, GL_READ_WRITE, GL_R32UI);
+
+    GLuint workGroupX = std::ceil(leafCount / 512.0);
+    glDispatchCompute(workGroupX, 1, 1);
+    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+    //need to generate next layer of "leaves"
+
 }
 
 FilterOctree::FilterOctree()
--- GlobalIllumination/FilterOctree.h
+++ GlobalIllumination/FilterOctree.h
@@ -3,6 +3,7 @@
 #include "GLBufferObject.h"
 #include "LogStruct.h"
 #include "ShaderProgram.h"
+#include "CounterBlock.h"
 class FilterOctree
 {
 private:
@@ -11,7 +12,7 @@ private:
     bool hasInitialized = false;
 public:
     void initialize();
-    void run(GLBufferObject<NodeStruct> & nodeOctree, GLBufferObject<GLuint> & ssboLeafIndexList, GLuint textureColor, GLuint textureNormal);
+    void run(GLBufferObject<CounterBlock>& counterSet, GLBufferObject<NodeStruct> & nodeOctree, GLBufferObject<GLuint> & ssboLeafIndexList, GLuint textureColor, GLuint textureNormal, GLuint leafCount);
     FilterOctree();
     ~FilterOctree();
 };
